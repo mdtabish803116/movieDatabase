ef13ff0b4a34a7b2ee4bcace715d351ab9271093
mdtabish803116 <mdtabish803116@gmail.com>
2022-07-20 10:59:44 +0530

    added modified files

--- handlers/movie.js
+++ handlers/movie.js
@@ -1,17 +1,91 @@
+const { captureRejectionSymbol } = require("events");
 const {movieModel} = require("../database/movie");
 
 async function fetchAllMovie(req , res , next){
-     let filter = req.query.filter||"All";
+     let {filterByTitle , filterByRating , searchByTitle , sortBy , page , limit} = req.query;
+     page = parseInt(page)-1||0;
+     limit = parseInt(limit)||5;
 
-     console.log(filter.split(","));
+     // console.log( filterByTitle.split(","))
+     // console.log(filterByRating.split(","))
 
-       let movies = {};
 
-     
+     /// filterByRating  
 
-         return res.send({
-             data : movies
-         })
+     if(filterByRating){
+          filterByRating =  {rating : {$in : filterByRating.split(",").map(Number)}}
+          console.log(filterByRating);
+     }else {
+            filterByRating = {}
+     }
+
+     ///filterByTitle
+
+       if(filterByTitle){
+           filterByTitle =  {title : {$in : filterByTitle.split(",")}}
+       }else {
+           filterByTitle = {}
+       }
+
+
+       //search query here
+
+
+       if(searchByTitle){
+          searchByTitle = {title : {$regex : searchByTitle , $options : "i"}}
+       }else {
+            searchByTitle = {}
+       }
+
+
+     /// sortings by
+
+       let sorting = {} // key-valuepair required thatswhy put it in a object 
+     if(sortBy){
+          sortBy = sortBy.split(",");
+          if(sortBy[1]){
+               if(sortBy[1] === "asc"){
+                     sorting[sortBy[0]] = 1
+               }else if(sortBy[1] === "dsc"){
+                      sorting[sortBy[0]] = -1
+               }else {
+                    return res.status(404).send({
+                          error : "Invalid sort"
+                    })
+               }
+               
+          }else {
+                sorting[sortBy[0]] = 1;
+          }
+     }else {
+          sorting = {}
+     }
+       try{  
+
+          let movies = await movieModel.find({$and : [filterByRating , filterByTitle , searchByTitle]})
+          .sort(sorting)
+          .skip(page*limit)
+          .limit(limit);
+
+          const total =  await movieModel.find({$and : [filterByRating , filterByTitle , searchByTitle]}).count()
+
+          console.log(movies);
+           return res.status(200).send({
+               message : "succesful",
+               total,
+               page : page + 1,
+               limit,
+               movies
+           })
+
+       }catch(err){
+            console.log("something went wrong",err);
+            return res.status(404).send({
+                 error : "Something went wrong"
+            })
+       }
+
+   
 }
 
 
